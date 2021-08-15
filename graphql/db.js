15b3bcfd03d315e558c043a539cda10933eb6c15
JhsK <qwe6293@nate.com>
2021-08-15 11:24:06 +0900

    rest API - graphql wrapper

--- graphql/db.js
+++ graphql/db.js
@@ -1,51 +1,47 @@
-let movies = [
-  {
-    id: 0,
-    name: "Star Wars - The new one",
-    score: 1
-  },
-  {
-    id: 1,
-    name: "Avengers - The new one",
-    score: 8
-  },
-  {
-    id: 2,
-    name: "The Godfather I",
-    score: 99
-  },
-  {
-    id: 3,
-    name: "Logan",
-    score: 2
-  }
-];
+import axios from "axios";
 
-export const getMovies = () => movies;
+const BASE_URL = "https://yts-proxy.now.sh/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === (id));
-    return filteredPeople[0];
-}
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating
+    }
+  });
+  return movies;
+};
 
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter(movie => movie.id !== (id));
+export const getMovie = async id => {
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return movie;
+};
 
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-}
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length - 1}`,
-    name,
-    score
-  };
-  movies.push(newMovie);
-  return newMovie;
-}
-
+export const getSuggestions = async id => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return movies;
+};
+  
